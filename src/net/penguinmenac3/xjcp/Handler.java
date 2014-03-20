a7ac8dfc6f401eec5289d74e3b10a8c8f177d55a
penguinmenac3 <penguinmenac3@gmail.com>
2014-03-20 16:55:35 +0100

    Completed interfaces. Only one error remaining that only concerns image and file transmission. The rest of the interface is perfectly working.

--- src/net/penguinmenac3/xjcp/Handler.java
+++ src/net/penguinmenac3/xjcp/Handler.java
@@ -5,6 +5,46 @@ package net.penguinmenac3.xjcp;
  * @version 1.0
  * @author Michael Fürst
  */
-public interface Handler {
-    void handleMessage(Message message);
+public abstract class Handler {
+    /**
+     * Subclasses must implement this to receive messages.
+     * @param msg The message to handle.
+     */
+    public abstract void handleMessage(final Message msg);
+
+    /**
+     * Returns a new Message from the global message pool.
+     * @return The message.
+     */
+    public final Message obtainMessage() {
+        return Message.obtain();
+    }
+
+    /**
+     * Same as obtainMessage(), except that it also sets the what, obj, arg1,and arg2 values on the returned Message.
+     * @return The message.
+     */
+    public final Message obtainMessage(final int what, final int arg1, final int arg2, final Object obj) {
+        Message m = Message.obtain();
+        m.arg1 = arg1;
+        m.arg2 = arg2;
+        m.obj = obj;
+        m.what = what;
+        return m;
+    }
+
+    /**
+     * Pushes a message onto the end of the message queue after all pending messages before the current time.
+     * @param msg The message to send.
+     * @return Weather the message was send or not.
+     */
+    public final boolean sendMessage(final Message msg) {
+        final Handler that = this;
+        new Thread() {
+            public void main() {
+                that.handleMessage(msg);
+            }
+        }.start();
+        return true;
+    }
 }
--- src/net/penguinmenac3/xjcp/Message.java
+++ src/net/penguinmenac3/xjcp/Message.java
@@ -6,7 +6,26 @@ package net.penguinmenac3.xjcp;
  * @version 1.0
  */
 public class Message {
-    // TODO make this conform to android impl.
-    public int type;
-    public Object[] args;
+    /**
+     * User-defined message code so that the recipient can identify what this message is about.
+     */
+    public int what;
+    /**
+     * An arbitrary object to send to the recipient.
+     */
+    public Object obj;
+    /**
+     * arg1 and arg2 are lower-cost alternatives to using setData() if you only need to store a few integer values.
+     */
+    public int arg1;
+    /**
+     * arg1 and arg2 are lower-cost alternatives to using setData() if you only need to store a few integer values.
+     */
+    public int arg2;
+    /**
+     * Optional Messenger where replies to this message can be sent.
+     */
+    public Object replyTo;
+
+    public static Message obtain() {return new Message();}
 }
