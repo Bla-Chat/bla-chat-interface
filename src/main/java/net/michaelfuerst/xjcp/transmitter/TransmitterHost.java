6a6f012cca24ba36ce1b4f794187906758ae9670
Daniel Wieland <d.wieland@arcor.de>
2014-06-07 12:19:32 +0200

    Added function to transmit only using WLAN/LAN.

--- src/main/java/net/michaelfuerst/xjcp/transmitter/TransmitterHost.java
+++ src/main/java/net/michaelfuerst/xjcp/transmitter/TransmitterHost.java
@@ -19,6 +19,8 @@ public final class TransmitterHost implements Transmitter {
 	
 	private final Transmitter[] transmitter;
 	
+	private boolean useOnlyLAN;
+	
 	/**
 	 * Creates a new TransmitterHost.
 	 * The host will hold a {@link DefaultTransmitter} and a {@link LANTransmitter}.
@@ -34,7 +36,18 @@ public final class TransmitterHost implements Transmitter {
 		this.transmitter[0] = new DefaultTransmitter(connection, parser, 500);
 		this.transmitter[1] = new LANTransmitter(connection, parser, 5000);
 	}
-
+	
+	/**
+	 * Changes weather we use the {@link DefaultTransmitter} and the 
+	 * {@link LANTransmitter} for large packets or if we only use the 
+	 * {@link LANTransmitter} to transmit.
+	 * 
+	 * @param wlanOnly True if we should only use WLAN/LAN-Connection.
+	 */
+	public void setWLANOnly(boolean wlanOnly) {
+		useOnlyLAN = wlanOnly;
+	}
+	
 	@Override
 	public void submit(String message, Handler handler) {
 		if (message == null) {
@@ -43,7 +56,7 @@ public final class TransmitterHost implements Transmitter {
 		
 		int size = message.getBytes().length;
 		
-		if (size <= MOBILEPACKETLIMIT) {
+		if (size <= MOBILEPACKETLIMIT && !useOnlyLAN) {
 			transmitter[DEFAULT_TRANSMITTER].submit(message, handler);
 		} else {
 			transmitter[LAN_TRANSMITTER].submit(message, handler);
